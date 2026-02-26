543f88310942d7dbd11c3001e52e4f32dfdcd878
samsonkuchipudi <samsonkuchipudi@gmail.com>
2026-02-27 00:40:12 +0100

    first API test

---
+++ test/api/test_basic_jsonplaceholder.py
@@ -0,0 +1,22 @@
+from src.api.clients.jsonplaceholder_client import Jsonplaceholderclient
+
+def test_list_users(base_url):
+    #triggering api and saving the response directly 
+    # response = requests.get(f"{base_url}/posts/1")
+    #Validating the status code of Get request.
+
+    client = Jsonplaceholderclient(base_url)
+    response = client.list_users(1)
+    assert response.status_code == 200
+
+    body = response.json()
+    print(body)
+
+    #All test validations of API
+    # assert "userId" in body
+    # assert body["userId"] == 1
+    # assert "id" in body
+    # assert body["id"] == 1
+    # assert "title" in body
+    # assert body["title"] == "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+    
